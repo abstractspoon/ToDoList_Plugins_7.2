3662796b5425dc1050e229bcb27c9010b1149b79
abstractspoon <daniel.godson@gmail.com>
2019-06-30 16:20:55 +1000

    Fix task reslection after preferences change

--- UIExension/DayViewUIExtension/DayViewUIExtensionCore/TDLDayView.cs
+++ UIExension/DayViewUIExtension/DayViewUIExtensionCore/TDLDayView.cs
@@ -242,8 +242,13 @@ namespace DayViewUIExtension
 			
 			// Notify parent of changes
 			if (allowNotify && (GetSelectedTaskID() != prevSelTaskID))
-                RaiseSelectionChanged();
-        }
+			{
+				CalendarItem item = null;
+				m_Items.TryGetValue(m_VisibleSelectedTaskID, out item);
+
+				RaiseSelectionChanged(item);
+			}
+		}
 
 		public bool SelectTask(UInt32 dwTaskID)
 		{
